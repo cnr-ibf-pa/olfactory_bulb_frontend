30a4ff06ec87888fec9e7ed2925689a67f3c366c
lbologna <lucaleonardo.bologna@gmail.com>
2022-05-11 15:00:32 +0200

    added auth.js

---
+++ src/handlers/auth.js
@@ -0,0 +1,73 @@
+import  Oidc from 'oidc-client';
+
+// comes from rollup.config.js
+// declare var processEnvs: any
+
+function createAuthConfig() {
+  const redirectBase = window.location.origin;
+  const settings = {
+    authority: 'https://iam.ebrains.eu/auth/realms/hbp',
+    authorization_endpoint: 'https://iam.ebrains.eu/auth/realms/hbp/protocol/openid-connect/auth', 
+    userinfo_endpoint: 'https://iam.ebrains.eu/auth/realms/hbp/protocol/openid-connect/userinfo',
+    client_id: 'localhost-test-2',
+    redirect_uri: 'http://localhost:8080/callback.html',
+    scope: 'openid profile email',
+    
+    respose_type: 'id_token token',
+    userStore: new Oidc.WebStorageStateStore({ store : window.localStorage }),
+  }
+  return settings;
+}
+
+async function login(authMgr) {
+  saveUrl(window.location.href);
+  return authMgr.signinRedirect();
+}
+
+async function loginSilent() {
+  const authMgr = createAuthManager();
+  await authMgr.signinSilent();
+}
+
+function createAuthManager() {
+  const oidcConfig = createAuthConfig();
+  const authMgr = new Oidc.UserManager(oidcConfig);
+  return authMgr;
+}
+
+function init() {
+  const authMgr = createAuthManager();
+  return login(authMgr);
+}
+
+async function getUserInfo() {
+  const authMgr = createAuthManager();
+  const user = await authMgr.getUser();
+  return user;
+}
+
+
+export const storageKeys = {
+  SAVED_URL: 'urlParam',
+  SELECTED_USECASE: 'selectedUsecase',
+  RETURN_LOGIN: 'returnFromLogin',
+  MODELS_LIST: 'modelsList',
+  SELECTED_COLLAB: 'selectedCollab',
+  SELECTED_CATEGORY: 'selectedCategory',
+};
+
+export function saveUrl(url) {
+  sessionStorage.setItem(storageKeys.SAVED_URL, url);
+}
+export function getSavedUrl() {
+  return sessionStorage.getItem(storageKeys.SAVED_URL);
+}
+
+
+export default init;
+
+export {
+  init,
+  getUserInfo,
+  loginSilent,
+}
